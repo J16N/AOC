46ae5c53ee8b076223dc5ead5d4290ce97435ebb
Tsubasa <scientistjishan@gmail.com>
2022-12-03 21:55:10 +0530

    Added solution to part 1 of the problem 1.

---
+++ 2022/q01a.py
@@ -0,0 +1,8 @@
+from aocd.models import Puzzle
+puzzle = Puzzle(year=2022, day=1)
+
+calories = [
+    sum(map(int, lines.splitlines()))
+    for lines in puzzle.input_data.split("\n\n")
+]
+puzzle.answer_a = max(calories)
